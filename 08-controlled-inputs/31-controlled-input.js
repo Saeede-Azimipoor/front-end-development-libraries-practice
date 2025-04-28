680837c6816f7ec812a877c42d0e8810f3719eb9
Saeede Azimipoor <106907243+Saeede-Azimipoor@users.noreply.github.com>
2025-04-28 13:31:39 -0700

    Delete 08-controlled-inputs directory

--- 08-controlled-inputs/31-controlled-input.js
+++
@@ -1,9 +0,0 @@
-const ControlledInput = () => {
-  const [input, setInput] = React.useState("");
-  return (
-    <div>
-      <input value={input} onChange={(e) => setInput(e.target.value)} />
-      <p>{input}</p>
-    </div>
-  );
-};
