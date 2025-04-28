2e3a1c5c5515d8f95d86c723e4f726c357c7eb46
Saeede Azimipoor <106907243+Saeede-Azimipoor@users.noreply.github.com>
2025-04-28 13:31:02 -0700

    Delete 06-state directory

--- 06-state/20-setState.js
+++
@@ -1,9 +0,0 @@
-class MyComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: "Saeede" };
-  }
-  render() {
-    return <h1>{this.state.name}</h1>;
-  }
-}
--- 06-state/21-initialize-state.js
+++
@@ -1,9 +0,0 @@
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { message: "Hello" };
-  }
-  render() {
-    return <p>{this.state.message}</p>;
-  }
-}
